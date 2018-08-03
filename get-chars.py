33f50f3d1dd0b80fe148a0e4ef978b18996e976f
Eli Heuer <eliheuer@gmail.com>
2018-08-03 14:54:21 -0400

    fix doc string

--- get-chars.py
+++ get-chars.py
@@ -1,3 +1,9 @@
+#MenuTitle: Print Chars
+# -*- coding: utf-8 -*-
+__doc__="""
+Print all chars from the font, useful for making specimens.
+"""
+
 for f in Glyphs.fonts:
     for g in f.glyphs:
         print g.string,
